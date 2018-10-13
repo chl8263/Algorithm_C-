2a0835c333064e02c72a8624ef5241dfa78d03f5
gyun-home <chl8263@naver.com>
2018-10-13 17:26:27 +0900

    recursion

---
+++ Algorithm/sequential_search/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int search(int data[], int n, int target) {
+	for (int i = 0; i < n; i++) {
+		if (data[i] == target) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int search_recursion(int data[], int begin, int end, int target) {
+
+	if (begin > end) return -1;
+
+	else if (target == data[begin])
+		return begin;
+
+	else return search_recursion(data, begin + 1, end, target);
+
+
+
+}
+
+int main() {
+	int data[] = { 1,2,3,4,5 };
+
+	//cout << search(data, 5, 3);
+	cout << search_recursion(data, 0, 4, 3);
+
+}
