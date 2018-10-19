ca0719d27e776513f04f38555ee54bf53a8d2e13
gyun-home <chl8263@naver.com>
2018-10-20 01:49:31 +0900

    dp

--- Algorithm/dp-nxn(memoization)/main.cpp
+++ Algorithm/dp-nxn(memoization)/main.cpp
@@ -10,26 +10,41 @@ int grid[4][4] = {
 	{ 8,10,14,9 }
 };
 
-int memo[4][4] = {
+int memo[5][5] = {
 	{ -1,-1,-1,-1 },
 	{ -1,-1,-1,-1 },
 	{ -1,-1,-1,-1 },
 	{ -1,-1,-1,-1 }
 };
 
-int mat(int i, int j) {
+int mat_topdown(int i, int j) {
 	if (memo[i][j] != -1) return memo[i][j];
 	if (i == 0 && j == 0)
 		memo[i][j] = grid[i][j];
 	else if (i == 0)
-		memo[i][j] = mat(0, j - 1) + grid[i][j];
+		memo[i][j] = mat_topdown(0, j - 1) + grid[i][j];
 	else if (j == 0)
-		memo[i][j] = mat(i - 1, 0) + grid[i][j];
+		memo[i][j] = mat_topdown(i - 1, 0) + grid[i][j];
 	else
-		memo[i][j] = min(mat(i-1, j),mat(i,j-1))+grid[i][j];
+		memo[i][j] = min(mat_topdown(i-1, j), mat_topdown(i,j-1))+grid[i][j];
 	
 	return memo[i][j];
+}
 
+int mat_bottomup(int a, int b) {
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			if (i == 0 && j == 0)
+				memo[i][j] = grid[0][0];
+			else if (i == 0)
+				memo[i][j] = grid[0][j] + memo[0][j - 1];
+			else if (j == 0)
+				memo[i][j] = grid[i][0] + memo[i - 1][0];
+			else
+				memo[i][j] = grid[i][j] + min(memo[i - 1][j], memo[i][j - 1]);
+		}
+	}
+	return memo[a-1][b-1];
 }
 
 int main() {
@@ -38,5 +53,6 @@ int main() {
 
 	cin >> i >> j;
 
-	cout << mat(i, j);
+	//cout << mat_topdown(i, j);
+	cout << mat_bottomup(i, j);
 }
